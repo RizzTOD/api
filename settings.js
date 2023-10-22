3c49e5c6e3d158c15888630f819fd073a4beb1fe
RizzTOD <139517025+RizzTOD@users.noreply.github.com>
2023-10-22 07:55:31 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'ritzmd'// yourname
+global.MONGO_DB_URI = "mongodb+srv://brutallegends68:tfaASMnDnycAQmW6@cluster0.trkach3.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "xrtod16@gmail.com" //email
+global.email_password = "akurizki098" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
